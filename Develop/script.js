3985eb961d9303ab4943b6558068d7aec01b6450
jschuyl <schuylertjed@gmail.com>
2022-08-18 18:02:53 -0700

    completed function, but not working so not completed. added notes to get hte idea of what I want to happen

--- Develop/script.js
+++ Develop/script.js
@@ -11,12 +11,19 @@ var passArray = [char, charCAPS, numbers, specials];
 
 
 function generatePassword() {
-  let newPass = ""
+  let newPass = "";
+  // loops generatePassword for however long the password is 
   for (let i = 0; i < passwordLength; i++) {
-     var password = Math.floor(Math.random()*char.passwordLength + 1);
-      newPass += char.charAt(password)
+    // this says give me a value between zero and three, or zero and however many options we are using
+      var arrayNumber = Math.floor(Math.random() * passArray.length);
+      // applies which options we chose
+      var whatOptions = passArray[arrayNumber];
+      // creates a random number that falls on the array, plus one to get to 26 if necessary
+      var password = Math.floor(Math.random() * whatOptions.length + 1);
+      // displays the random value chosen in passArray until the loop finishes
+      newPass = newPass + char.charAt(password);
   }
-  return newPass
+  return newPass;
 }
 console.log(password)
 // Write password to the #password input
