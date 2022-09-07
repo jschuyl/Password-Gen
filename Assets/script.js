eb7e6958c8f00ef64bb6261ba8a8cd76e2bb6d4e
jschuyl <schuylertjed@gmail.com>
2022-09-07 14:00:58 -0700

    made the page stop the function if the passwrod length is too short/long, rearranged some code and added notes to make it easier to read

--- Assets/script.js
+++ Assets/script.js
@@ -12,13 +12,23 @@ console.log(passArray)
 
 function generatePassword() {
   let newPass = ""
-
+// lets user pick length of password, also stops function if user uses less or more than they're allowed to
   let pickyLength = prompt("How many characters 8-128?")
+  if(pickyLength < 8) {
+    alert("Please use at least 8 characters");
+    return;
+  } if(pickyLength > 128) {
+    alert("Please use no more than 128 characters");
+    return;
+  }
+// lets user choose the parameters 
   let useChar = confirm("Use lowercase letters?")
   let useCaps = confirm("Use capital letters?")
   let useNum = confirm("Use Numbers?")
   let useSpec = confirm("Use special characters?")
-  if (useChar === true) {
+  
+
+   if (useChar === true) {
     passArray.push(char)
   } if (useCaps === true)  {
     passArray.push(charCaps)
@@ -26,17 +36,8 @@ function generatePassword() {
     passArray.push(numbers)
   } if (useSpec === true) {
     passArray.push(specials)
-  } if (useChar === false, useCaps === false, useNum === false, useSpec === false) {
+  } if (passArray.length === 0) {
     alert("Please pick at least one option, I can't make something out of nothing");
-    return;
-  }
-  console.log(useChar, useCaps, useNum, useSpec)
-
-  if(pickyLength < 8) {
-    alert("Please use at least 8 characters");
-    return;
-  } if(pickyLength > 128) {
-    alert("Please use no more than 128 characters");
     return;
   } else {
     for (i = 0; i < pickyLength; i++) {
